104b3f2c6e3c7e7179dd10b87e61e85e7fd1dc20
feng.yu <f1768019@gmail.com>
2019-05-20 16:45:02 -0400

    add implementation using heap, which is good at return the min

--- may19.py
+++ may19.py
@@ -4,7 +4,7 @@ return a new sorted merged list from K sorted lists, each with size N
 For example, if we had [[10, 15, 30], [12, 15, 20], [17, 20, 32]],
 the result should be [10, 12, 15, 15, 17, 20, 20, 30, 32]
 """
-
+import heapq
 
 def merge_sorted_list(list_sorted_lists):
     merged_list = list_sorted_lists[0]
@@ -21,8 +21,26 @@ def merge_sorted_list(list_sorted_lists):
     return merged_list
 
 
+def merg_sorted_list2(list_sorted_lists):
+    merged_list = []
+    heap = [(lis[0], count, 0) for count, lis in enumerate(list_sorted_lists) if lis]
+    heapq.heapify(heap)
+
+    while heap:
+        val, list_index, ele_index = heapq.heappop(heap)
+        merged_list.append(val)
+
+        if ele_index + 1 < len(list_sorted_lists[list_index]):
+            heapq.heappush(heap, (list_sorted_lists[list_index][ele_index + 1],
+                                  list_index,
+                                  ele_index + 1
+                                  ))
+    return merged_list
+
+
 test_data = [[10, 15, 30], [12, 15, 20], [17, 20, 32]]
 expected_result = [10, 12, 15, 15, 17, 20, 20, 30, 32]
-result = merge_sorted_list(test_data)
+# result = merge_sorted_list(test_data)
+result = merg_sorted_list2(test_data)
 print(result)
 assert result == expected_result
